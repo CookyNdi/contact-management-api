312028d0da815c3b1ee2b612e45941ac13b50b56
CookyNdi <73926548+CookyNdi@users.noreply.github.com>
2024-07-07 09:15:50 +0700

    test : unit test contact

--- test/test.service.ts
+++ test/test.service.ts
@@ -1,18 +1,35 @@
 import { PrismaService } from '../src/common/prisma.service';
 import { Injectable } from '@nestjs/common';
-import { User } from '@prisma/client';
 import * as bcrypt from 'bcrypt';
+import { Contact, User } from '@prisma/client';
 
 @Injectable()
 export class TestService {
   constructor(private prismaService: PrismaService) {}
 
   async deleteAll() {
+    await this.deleteContact();
     await this.deleteUser();
   }
 
   async deleteUser() {
     await this.prismaService.user.deleteMany({
+      where: {
+        username: 'test',
+      },
+    });
+  }
+
+  async deleteContact() {
+    await this.prismaService.contact.deleteMany({
+      where: {
+        username: 'test',
+      },
+    });
+  }
+
+  async getUser(): Promise<User> {
+    return this.prismaService.user.findUnique({
       where: {
         username: 'test',
       },
@@ -30,9 +47,21 @@ export class TestService {
     });
   }
 
-  async getUser(): Promise<User> {
-    return this.prismaService.user.findUnique({
+  async getContact(): Promise<Contact> {
+    return this.prismaService.contact.findFirst({
       where: {
+        username: 'test',
+      },
+    });
+  }
+
+  async createContact() {
+    await this.prismaService.contact.create({
+      data: {
+        first_name: 'test',
+        last_name: 'test',
+        email: 'test@example.com',
+        phone: '9999',
         username: 'test',
       },
     });
